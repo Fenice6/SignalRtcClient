0901c0011d6dde366963af6e7c62e2fa47dd7b13
Francesco Lapiana <fenice-6@libero.it>
2020-02-25 17:30:13 +0100

    [Update] Signalr service: startConnection
    It's possible start an hub connection. The endpoint is loaded from configurations file.

--- signalRtc/src/app/signalr.service.ts
+++ signalRtc/src/app/signalr.service.ts
@@ -3,6 +3,7 @@ import * as signalR from "@aspnet/signalr";
 import { Subject } from 'rxjs';
 import { User } from 'src/models/user.interface';
 import { Signal } from 'src/models/signal.interface';
+import { apiEndpoint } from 'src/config';
 
 @Injectable({
   providedIn: 'root'
@@ -25,4 +26,32 @@ export class SignalrService {
 
   constructor() { }
   
+  public async startConnection(currentUser: string): Promise<void> {
+
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl(`${apiEndpoint}/signalrtc`)
+      .build();
+
+    await this.hubConnection.start();
+    console.log('Connection started');
+
+    this.hubConnection.on('NewUserArrived', (data) => {
+      this.newPeer.next(JSON.parse(data));
+    });
+
+    this.hubConnection.on('UserSaidHello', (data) => {
+      this.helloAnswer.next(JSON.parse(data));
+    });
+
+    this.hubConnection.on('UserDisconnect', (data) => {
+      this.disconnectedPeer.next(JSON.parse(data));
+    });
+
+    this.hubConnection.on('SendSignal', (user, signal) => {
+      this.signal.next({ user, signal });
+    });
+
+    this.hubConnection.invoke('NewUser', currentUser);
+  }s
+
 }
