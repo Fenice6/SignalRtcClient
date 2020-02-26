e8f053359242ec4ad10ed7b91e6d188cdbaf22fc
Francesco Lapiana <fenice-6@libero.it>
2020-02-26 15:52:03 +0100

    [Update] Component: app (logic)
    Logic is on ngOnInit function. All observable are subribed then actions are defined and dispatched.

--- signalRtc/src/app/app.component.ts
+++ signalRtc/src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { RtcService } from './rtc.service';
+import { Subscription } from 'rxjs';
+import { SignalrService } from './signalr.service';
+import { PeerData } from 'src/models/peerData.interface';
+import { User } from 'src/models/user.interface';
+import { Signal } from 'src/models/signal.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'signalRtc';
+export class AppComponent implements OnInit, OnDestroy {
+  
+  @ViewChild('videoPlayer', { static: false }) videoPlayer: ElementRef;
+
+  public subscriptions = new Subscription();
+
+  private stream;
+
+  public currentUser: string;
+
+  public dataString: string;
+
+  public userVideo: string;
+
+  public mediaError = (): void => { console.error(`Can't get user media`); };
+
+  constructor(private rtcService: RtcService, private signalR: SignalrService) { }
+
+  ngOnInit() {
+    this.subscriptions.add(this.signalR.newPeer$.subscribe((user: User) => {
+      this.rtcService.newUser(user);
+      this.signalR.sayHello(this.currentUser, user.connectionId);
+    }));
+
+    this.subscriptions.add(this.signalR.helloAnswer$.subscribe((user: User) => {
+      this.rtcService.newUser(user);
+    }));
+
+    this.subscriptions.add(this.signalR.disconnectedPeer$.subscribe((user: User) => {
+      this.rtcService.disconnectedUser(user);
+    }));
+
+    this.subscriptions.add(this.signalR.signal$.subscribe((signalData: Signal) => {
+      this.rtcService.signalPeer(signalData.user, signalData.signal, this.stream);
+    }));
+
+    this.subscriptions.add(this.rtcService.onSignalToSend$.subscribe((data: PeerData) => {
+      this.signalR.sendSignalToUser(data.data, data.id);
+    }));
+
+    this.subscriptions.add(this.rtcService.onData$.subscribe((data: PeerData) => {
+      console.log(`Data from user ${data.id}: ${data.data}`);
+    }));
+
+    this.subscriptions.add(this.rtcService.onStream$.subscribe((data: PeerData) => {
+      this.userVideo = data.id;
+      this.videoPlayer.nativeElement.srcObject = data.data;
+      this.videoPlayer.nativeElement.load();
+      this.videoPlayer.nativeElement.play();
+    }));
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 }
