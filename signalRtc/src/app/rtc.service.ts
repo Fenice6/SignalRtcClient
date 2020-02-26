0f03350f74b502c36cc7a40d463642c87317ac7c
Francesco Lapiana <fenice-6@libero.it>
2020-02-26 10:51:54 +0100

    [Update] Rtc service
    Structure to maintains lit of connected user and peers.

--- signalRtc/src/app/rtc.service.ts
+++ signalRtc/src/app/rtc.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
+import { Instance } from 'simple-peer';
+import { PeerData } from 'src/models/peerData.interface';
+import { User } from 'src/models/user.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RtcService {
 
-  constructor() { }
+  private users: BehaviorSubject<Array<User>>;
+  public users$: Observable<Array<User>>;
+
+  private onSignalToSend = new Subject<PeerData>();
+  public onSignalToSend$ = this.onSignalToSend.asObservable();
+
+  private onStream = new Subject<PeerData>();
+  public onStream$ = this.onStream.asObservable();
+
+  private onConnect = new Subject<PeerData>();
+  public onConnect$ = this.onConnect.asObservable();
+
+  private onData = new Subject<PeerData>();
+  public onData$ = this.onData.asObservable();
+
+  public currentPeer: Instance;
+
+  constructor() { 
+    this.users = new BehaviorSubject([]);
+    this.users$ = this.users.asObservable();
+  }
+
 }
